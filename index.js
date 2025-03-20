7034a36cd0b664d4201d887da86656c927287a40
shantanupaul81 <paulshantanu18@gmail.com>
2025-03-20 11:47:59 +0530

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import connectDB from './config/db.js';
 
 dotenv.config();
 const app = express();
-const PORT =  9000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
